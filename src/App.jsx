db38eaa545fb8d89d920679fe58ce322699e7c6d
Daris02 <hei.raymond.2@gmail.com>
2023-08-08 17:30:31 +0300

    fix: update import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import Login from './Login';
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './SignUp';
+import Home from './pages/Home';
+import Message from './pages/Message';
+import About from './pages/About';
 
 function App() {
-  return (
-    <>
-      <Login />
-      {/* <SignUp /> */}
-    </>
-  );
+
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Login />} />
+				<Route path='/signup' element={<SignUp/>} />
+				<Route path='/home' element={<Home />} />
+				<Route path='/message' element={<Message />} />
+				<Route path='/about' element={<About />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
