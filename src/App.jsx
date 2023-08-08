c775ee8aef6c2ba758f56023ede90aea8345575d
Daris02 <hei.raymond.2@gmail.com>
2023-08-08 21:21:20 +0300

    fix{router}: edit new path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './Login';
 import { Routes, Route } from 'react-router-dom';
 import SignUp from './SignUp';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Message from './pages/Message';
 import About from './pages/About';
@@ -13,9 +14,11 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Login />} />
 				<Route path='/signup' element={<SignUp/>} />
-				<Route path='/home' element={<Home />} />
-				<Route path='/message' element={<Message />} />
-				<Route path='/about' element={<About />} />
+				<Route path='/user' element={<Header />}>
+					<Route path='/user/home' element={<Home />} />
+					<Route path='/user/message' element={<Message />} />
+					<Route path='/user/about' element={<About />} />
+				</Route>
 			</Routes>
 		</>
 	);
