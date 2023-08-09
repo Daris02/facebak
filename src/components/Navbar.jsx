e2feb67cf75c80648ce13fd627b7cc9367e55c0a
Daris02 <hei.raymond.2@gmail.com>
2023-08-09 11:59:59 +0300

    fix: update message icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,50 @@
 import React from 'react';
-import { CDBSidebar, CDBSidebarContent, CDBSidebarHeader, CDBSidebarMenu, CDBSidebarMenuItem, CDBSidebarFooter } from 'cdbreact';
+import { CDBSidebar, CDBSidebarContent, CDBSidebarHeader, CDBSidebarMenu, CDBSidebarMenuItem, CDBSidebarFooter, CDBIcon } from 'cdbreact';
 import { Link, Outlet } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <div className="d-flex vh-100">
-      <CDBSidebar>
-			<CDBSidebarHeader prefix={<i className="fa fa-bars" />}>Menu</CDBSidebarHeader>
-			<CDBSidebarContent>
-			<CDBSidebarMenu>
-				<Link to="/user/home" className="text-decoration-none">
-					<CDBSidebarMenuItem icon="th-large">
-						Home
-					</CDBSidebarMenuItem>
-				</Link>
-				<Link to="/user/message" className="text-decoration-none">
-					<CDBSidebarMenuItem icon="sticky-note">
-						Message
-					</CDBSidebarMenuItem>
-				</Link>
-				<Link to="/user/about" className="text-decoration-none">
-					<CDBSidebarMenuItem icon="credit-card" iconType="solid">
-						About Us
-					</CDBSidebarMenuItem>
-				</Link>
-				<Link to="/" className="text-decoration-none">
-					<CDBSidebarMenuItem icon="credit-card" iconType="solid">
-						Log Out
-					</CDBSidebarMenuItem>
-				</Link>
-			</CDBSidebarMenu>
-			</CDBSidebarContent>
+	return (
+		<div className="d-flex vh-100">
+			<div id="navbar">
+				<CDBSidebar>
+					<CDBSidebarHeader prefix={<i className="fa fa-bars" />}>FACEBAK</CDBSidebarHeader>
+					<CDBSidebarContent>
+						<CDBSidebarMenu>
+							<Link to="/user/home" className="text-decoration-none">
+								<CDBSidebarMenuItem icon="home">
+									Home
+								</CDBSidebarMenuItem>
+							</Link>
+							<Link to="/user/message" className="text-decoration-none">
+								<CDBSidebarMenuItem icon="comments">
+									Message
+								</CDBSidebarMenuItem>
+							</Link>
+							<Link to="/user/about" className="text-decoration-none">
+								<CDBSidebarMenuItem icon="info">
+									About Us
+								</CDBSidebarMenuItem>
+							</Link>
+							<Link to="/" className="text-decoration-none">
+								<CDBSidebarMenuItem icon="user">
+									Log Out
+								</CDBSidebarMenuItem>
+							</Link>
+						</CDBSidebarMenu>
+					</CDBSidebarContent>
 
-			<CDBSidebarFooter style={{ textAlign: 'center' }}>
-			<div className="sidebar-btn-wrapper" style={{padding: '20px 5px'}}>
-				facebak ChatApp
+					<CDBSidebarFooter style={{ textAlign: 'center' }}>
+					<div className="sidebar-btn-wrapper" style={{padding: '20px 5px'}}>
+						ChatApp
+					</div>
+					</CDBSidebarFooter>
+				</CDBSidebar>
 			</div>
-			</CDBSidebarFooter>
-		</CDBSidebar>
-		<div className="container">
-			<Outlet />
+			<div className="container">
+				<Outlet />
+			</div>
 		</div>
-    </div>
-  );
+	);
 };
 
 export default Navbar;
