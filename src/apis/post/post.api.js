ad85b4626252e0fa1cd3cd34eefcc8c553ecc709
Daris02 <hei.raymond.2@gmail.com>
2023-08-09 12:52:37 +0300

    feat: add fetching data

--- src/apis/post/post.api.js
+++ src/apis/post/post.api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
+const baseURL = 'http://localhost:8080';
 
-export function getAllPosts() {
-    
+export async function getAllPosts() {
+    try {
+        const { data } = await axios.get(`${baseURL}/posts`);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function addPost() {
