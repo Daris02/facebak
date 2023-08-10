5e04a37bf92c2feeaa047d67b1ced928c6907575
Daris02 <hei.raymond.2@gmail.com>
2023-08-10 16:50:42 +0300

    fix: update title

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className="container">
-            <h2>About Us</h2>
+            <h2>Notification</h2>
         </div>
     );
 }
