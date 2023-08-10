474fa682be16da6230dd3d88699cad4606911a9a
Daris02 <hei.raymond.2@gmail.com>
2023-08-10 16:12:12 +0300

    fix: add template

--- src/apis/user/user.apis.js
+++ src/apis/user/user.apis.js
@@ -5,8 +5,23 @@ export function getAllUsers() {
     
 }
 
-export function addUser() {
-
+export async function addUser() {
+    try {
+        const { data } = await axios.post(`${baseURL}/users`);
+        // Forme data :
+        // {
+        //     "email": "jane.smith@gmail.com",
+        //     "password": "12345678",
+        //     "confirmPassword": "12345678",
+        //     "username": "Jane Smith",
+        //     "joinedAt": "2023-08-10T14:40:53.853Z",
+        //     "bio": null,
+        //     "photo": null
+        // }
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export async function authUser(loginData) {
