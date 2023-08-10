d5208d65f1ac669276230b51959421024c10440d
Daris02 <hei.raymond.2@gmail.com>
2023-08-10 16:50:27 +0300

    fix: add icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as FA from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
 	return (
 		<>
-			<nav class="navbar navbar-expand-lg bg-body-tertiary">
-				<div class="container-fluid">
-					<Link to="/user/home" className="navbar-brand">
-							Logo
-					</Link>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item">
-							<Link to="/user/home" className="nav-link">
-								Home
-							</Link>
-						</li>
-						<li class="nav-item">
-							<Link to="/user/message" className="nav-link">
-								Message
-							</Link>
-						</li>
-						<li class="nav-item">
-							<Link to="/user/about" className="nav-link">
-								About Us
-							</Link>
-						</li>
-						<li class="nav-item">
-							<Link to="/" className="nav-link disabled">
-								Log Out
-							</Link>
-						</li>
-					</ul>
+			<nav className="navbar navbar-expand-lg bg-body-tertiary">
+				<div className="d-flex justify-content-between w-100">
+					<div className="w-25">
+						<Link to="/user/home" className="navbar-brand">
+								Logo
+						</Link>
+					</div>
+					<div className="w-25">
+						<form className="d-flex" role="search">
+						<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn btn-outline-primary" type="submit">Search</button>
+						</form>
+					</div>
+					<div className="w-25">
+						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon"></span>
+						</button>
+						<div className="collapse navbar-collapse" id="navbarNav">
+							<ul className="navbar-nav">
+								<li className="nav-item">
+									<Link to="/user/home" className="nav-link">
+										<FontAwesomeIcon icon={FA.faPlusCircle} size="lg" />
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link to="/user/message" className="nav-link">
+										<FontAwesomeIcon icon={FA.faMessage} /> Chat
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link to="/user/about" className="nav-link">
+										<FontAwesomeIcon icon={FA.faBell} /> Notification
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link to="/" className="nav-link disabled">
+										<FontAwesomeIcon icon={FA.faSignOutAlt} /> Log Out
+									</Link>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</nav>
