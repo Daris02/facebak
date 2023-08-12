e36f895986680ee35ba19e4fec2d387db61d963f
Daris02 <hei.raymond.2@gmail.com>
2023-08-12 09:45:05 +0300

    fix: separate sign-in and sign-up page

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { useForm } from 'react-hook-form';
 import { addUser } from "./apis/user/user.apis";
 
@@ -6,7 +7,6 @@ const SignUp = () => {
     const { register, handleSubmit, formState: { errors }, } = useForm({});
 
     const onSubmit = (data) => {
-        console.log(data);
         alert(data)
         // addUser(data).then(res => {
         //     if(res.status === 200) {
@@ -20,39 +20,35 @@ const SignUp = () => {
     };
 
     return (
-        <div className="modal fade" id="signUp" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content border border-warning">
-                    <div className="modal-header">
-                        <h1 className="modal-title mx-auto" id="signUpLabel">
-                            Create new account
-                        </h1>
+		<div className="login d-flex justify-content-center align-items-center vh-100 bg-primary-ligth">
+            <div className="p-5 rounded bg-white border border-warning">
+                <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column gap-2">
+                    <h2 className="text-center">Create new account</h2>
+                    <div className="mb-2">
+                        <label >User name</label>
+                        <input type="text" className="form-control" {...register("username", { required: true })}/>
                     </div>
-                    <div className="modal-body d-flex justify-content-around py-3">
-                        <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column gap-2">
-                            <div className="mb-4">
-                                <label >User name</label>
-                                <input type="text" className="form-control" {...register("username", { required: true })}/>
-                            </div>
-                            <div className="mb-4">
-                                <label >Email</label>
-                                <input type="email" className="form-control" {...register("email", { required: true })}/>
-                                { errors.name && ( <span className="text-danger">Emal required /!\</span>) }
-                            </div>
-                            <div className="mb-4">
-                                <label >Password</label>
-                                <input type="password" className="form-control" {...register("password", { required: true })}/>
-                            </div>
-                            <div className="mb-4">
-                                <label >Confirm password</label>
-                                <input type="password" className="form-control" {...register("confirmPassword", { required: true })}/>
-                            </div>
-                            <div className="d-grid my-3">
-                                <button type="submit" className="btn btn-outline-warning">Sign Up</button>
-                            </div>
-                        </form>
+                    <div className="mb-2">
+                        <label >Email</label>
+                        <input type="email" className="form-control" {...register("email", { required: true })}/>
+                        { errors.name && ( <span className="text-danger">Emal required /!\</span>) }
                     </div>
-                </div>
+                    <div className="mb-2">
+                        <label >Password</label>
+                        <input type="password" className="form-control" {...register("password", { required: true })}/>
+                    </div>
+                    <div className="mb-2">
+                        <label >Confirm password</label>
+                        <input type="password" className="form-control" {...register("confirmPassword", { required: true })}/>
+                    </div>
+                    <div className="d-grid my-4">
+                        <button type="submit" className="btn btn-outline-warning" id="signup">Sign Up</button>
+                    </div>
+                    <hr />
+                    <Link to="/">
+                        <button type="button" className="btn btn-outline-primary w-100">Sign In</button>
+                    </Link>
+                </form>
             </div>
         </div>
     );
