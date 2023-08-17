61c6767725a39479e6f81f163270971548bed1c1
Daris02 <hei.raymond.2@gmail.com>
2023-08-17 18:58:24 +0300

    feat: add message maket

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,7 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import { getAllUsers } from "../apis/user/user.apis";
+import { Container, Row, Col, ListGroup, Card, Form, Button } from 'react-bootstrap';
 
 const Message = () => {
+    const [selectedFriend, setSelectedFriend] = useState(null);
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    const handleFriendSelect = (friend) => {
+        setSelectedFriend(friend);
+        setMessages([]); // Réinitialiser les messages lorsque l'ami sélectionné change
+    };
+
+    const handleSendMessage = () => {
+        if (message.trim() === '') return;
+        const newMessage = { text: message, sender: 'user' };
+        setMessages([...messages, newMessage]);
+        setMessage('');
+    };
+
+
     const [friends, setFriends] =  useState([]);
     useEffect(() => {
         const fetchData = async () => {
@@ -9,6 +27,7 @@ const Message = () => {
                 const data = await getAllUsers();
                 console.log(data);
                 setFriends(data);
+                setSelectedFriend(data)
             } catch (error) {
                 console.error("Une erreur s'est produite :", error);
             }
@@ -19,9 +38,9 @@ const Message = () => {
     }, []);
 
     return (
-        <div className="container my-5 mx-auto w-75">
-            <h2 className="text-center">Message Page</h2>
-            <div className="row">
+        <div className="container my-5 pt-5 mx-auto w-75">
+            {/* <h2 className="text-center">Message</h2> */}
+            {/* <div className="row">
                 <div className="col-2 mx-2 border border-secondary">
                     <div className="container">
                         {
@@ -36,7 +55,61 @@ const Message = () => {
                 <div className="col-8 mw-2 border border-secondary">
 
                 </div>
-            </div>
+            </div> */}
+
+            <Container fluid className="app-container">
+                <Row>
+                    <Col md={4} className="friends-list">
+                        <ListGroup>
+                            {friends.map((friend) => (
+                                <ListGroup.Item
+                                    key={friend.id}
+                                    action
+                                    active={selectedFriend === friend}
+                                    onClick={() => handleFriendSelect(friend)}
+                                >
+                                    {friend.username}
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Col>
+
+                    <Col md={8} className="message-area">
+                        {selectedFriend ? (
+                            <Card>
+                                <Card.Header>{selectedFriend.username}</Card.Header>
+                                <Card.Body>
+                                    <div className="message-list">
+                                    {messages.map((msg, index) => (
+                                        <div key={index} className={`message ${msg.sender}`}>
+                                        {msg.text}
+                                        </div>
+                                    ))}
+                                    </div>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <Form>
+                                        <Form.Group className="mb-3">
+                                            <Form.Control
+                                            type="text"
+                                            placeholder="Votre message..."
+                                            value={message}
+                                            onChange={(e) => setMessage(e.target.value)}
+                                            />
+                                        </Form.Group>
+                                        <Button variant="primary" onClick={handleSendMessage}>
+                                            Envoyer
+                                        </Button>
+                                    </Form>
+                                </Card.Footer>
+                            </Card>
+                        ) : (
+                            <div className="select-friend-message">Sélectionnez un ami pour commencer la discussion</div>
+                        )}
+                    </Col>
+                </Row>
+            </Container>
+
         </div>
     );
 }
