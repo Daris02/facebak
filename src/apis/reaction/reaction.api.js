15d147ea4e6b98a7d88249cf386489430d936e85
Daris02 <hei.raymond.2@gmail.com>
2023-08-24 18:56:02 +0300

    feat: add new fetching data

--- src/apis/reaction/reaction.api.js
+++ src/apis/reaction/reaction.api.js
@@ -3,13 +3,32 @@ const baseURL = 'http://localhost:8080';
 
 export async function getReactionByIdPost(idPost) {
     try {
-        const { data } = await axios.get(`${baseURL}/${idPost}/reactions`);
+        const { data } = await axios.get(`${baseURL}/posts/${idPost}/reactions`);
         return data;
     } catch (error) {
         console.log(error);
     }
 }
 
-export async function changeReaction() {
+export async function addReaction(idUser, idPost, reactionType) {
+    try {
+        const { data } = await axios.post(`${baseURL}/posts/${idPost}/reactions`, {
+            userId: idUser,
+            type: reactionType
+        });
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+export async function deleteReaction(idUser, idPost) {
+    try {
+        const { data } = await axios.delete(`${baseURL}/posts/${idPost}/reactions`, {
+            userId: idUser
+        });
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
 }
