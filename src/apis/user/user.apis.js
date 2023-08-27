12991fd4a1ea5238d2ecef62aa5125c0ab277eb7
Daris02 <hei.raymond.2@gmail.com>
2023-08-27 20:27:56 +0300

    fix: optimise function api

--- src/apis/user/user.apis.js
+++ src/apis/user/user.apis.js
@@ -1,54 +1,13 @@
-import axios from 'axios';
+import { Get, Post } from '../baseApi';
 
-const baseURL = 'http://localhost:8080';
-
-export async function authUser({username, email, password}) {
-    try {
-        const { data } = await axios.post(
-                `${baseURL}/users/login`, 
-                {
-                    username,
-                    email,
-                    password
-                }
-            );
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+export function authUser({username, email, password}) {
+    return Post('/users/login', {username, email, password});
 }
 
 export async function getAllUsers() {
-    try {
-        const { data } = await axios.get(`${baseURL}/users`);
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+    return Get('/users');
 }
 
 export async function addUser({email, password, confirmPassword, username}) {
-    try {
-        const { data } = await axios.post(
-            `${baseURL}/users`,
-            {
-                email,
-                password,
-                confirmPassword,
-                username
-            }
-        );
-        // Forme data :
-        // {
-        //     "id": "3e52c217-d1da-4b1e-98fa-4ec7020d669f",
-        //     "email": "johndoe@gmail.com",
-        //     "username": "JohnDoe",
-        //     "joinedAt": "2023-08-27T15:54:33.425Z",
-        //     "bio": null,
-        //     "photo": "https://images.pexels.com/photos/17759970/pexels-photo-17759970/free-photo-of-bois-lumineux-route-paysage.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        //   }
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+    return Post('/users', {email, password, confirmPassword, username});
 }
