bcbd2956f6292aa838a63885d0111a0c35032c52
Daris02 <hei.raymond.2@gmail.com>
2023-08-27 19:49:13 +0300

    fix: update login and sign up params

--- src/apis/user/user.apis.js
+++ src/apis/user/user.apis.js
@@ -1,18 +1,26 @@
 import axios from 'axios';
+
 const baseURL = 'http://localhost:8080';
 
-export async function getAllUsers() {
+export async function authUser({username, email, password}) {
     try {
-        const { data } = await axios.get(`${baseURL}/users`);
+        const { data } = await axios.post(
+                `${baseURL}/users/login`, 
+                {
+                    username,
+                    email,
+                    password
+                }
+            );
         return data;
     } catch (error) {
         console.log(error);
     }
 }
 
-export async function authUser({email, password}) {
+export async function getAllUsers() {
     try {
-        const { data } = await axios.post(`${baseURL}/users/auth`, {email, password});
+        const { data } = await axios.get(`${baseURL}/users`);
         return data;
     } catch (error) {
         console.log(error);
@@ -32,14 +40,13 @@ export async function addUser({email, password, confirmPassword, username}) {
         );
         // Forme data :
         // {
-        //     "email": "jane.smith@gmail.com",
-        //     "password": "12345678",
-        //     "confirmPassword": "12345678",
-        //     "username": "Jane Smith",
-        //     "joinedAt": "2023-08-10T14:40:53.853Z",
+        //     "id": "3e52c217-d1da-4b1e-98fa-4ec7020d669f",
+        //     "email": "johndoe@gmail.com",
+        //     "username": "JohnDoe",
+        //     "joinedAt": "2023-08-27T15:54:33.425Z",
         //     "bio": null,
-        //     "photo": null
-        // }
+        //     "photo": "https://images.pexels.com/photos/17759970/pexels-photo-17759970/free-photo-of-bois-lumineux-route-paysage.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        //   }
         return data;
     } catch (error) {
         console.log(error);
