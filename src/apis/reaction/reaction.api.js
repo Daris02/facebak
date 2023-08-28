2ed663c3de1e5f451c8471b65e93c0ef6d4999d4
Daris02 <hei.raymond.2@gmail.com>
2023-08-28 14:19:05 +0300

    fix: change name parameter

--- src/apis/reaction/reaction.api.js
+++ src/apis/reaction/reaction.api.js
@@ -1,13 +1,13 @@
 import { Delete, Get, Post } from '../baseApi';
 
-export async function getReactionByIdPost(idPost) {
-    return Get(`/posts/${idPost}/reactions`, null);
+export async function getReactionByIdPost(postId) {
+    return Get(`/posts/${postId}/reactions`, null);
 }
 
-export async function addReaction(idUser, idPost, reactionType) {
-    return Post(`/posts/${idPost}/reactions`, {userId: idUser, type: reactionType});
+export async function addReaction(userId, postId, type) {
+    return Post(`/posts/${postId}/reactions`, {userId, type});
 }
 
-export async function deleteReaction(idUser, idPost) {
-    return Delete(`/posts/${idPost}/reactions`, {userId: idUser})
+export async function deleteReaction(userId, postId) {
+    return Delete(`/posts/${postId}/reactions`, {userId})
 }
