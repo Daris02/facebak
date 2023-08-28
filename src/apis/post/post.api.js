e167e20f4459ccfe0287e5f247d4dab5a6109822
Daris02 <hei.raymond.2@gmail.com>
2023-08-28 23:15:52 +0300

    feat: add create post Put method

--- src/apis/post/post.api.js
+++ src/apis/post/post.api.js
@@ -1,9 +1,9 @@
-import { Get } from '../baseApi';
+import { Get, Put } from '../baseApi';
 
 export async function getAllPosts() {
     return Get(`/posts`);
 }
 
-export function addPost() {
-
+export function addPost({title, content, userId}) {
+    return Put(`/posts`, {title, content, userId})
 }
